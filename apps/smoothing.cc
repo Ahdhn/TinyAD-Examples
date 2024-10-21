9f08cf0b2554d999bb8ef77e4ccd1c333d78d823
ahmed <a.has.mahmoud@gmail.com>
2024-10-21 10:00:34 -0400

    Update smoothing.cc

--- apps/smoothing.cc
+++ apps/smoothing.cc
@@ -10,15 +10,50 @@
 #include <TinyAD-Examples/Filesystem.hh>
 #include <TinyAD-Examples/GlowViewerOpenMesh.hh>
 
-int main()
+#include <chrono>
+
+struct CPUTimer
 {
+    CPUTimer()
+    {
+    }
+    ~CPUTimer()
+    {
+    }
+    void start()
+    {
+        m_start = std::chrono::high_resolution_clock::now();
+    }
+    void stop()
+    {
+        m_stop = std::chrono::high_resolution_clock::now();
+    }
+    float elapsed_millis()
+    {
+        return std::chrono::duration<float, std::milli>(m_stop - m_start)
+            .count();
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point m_start;
+    std::chrono::high_resolution_clock::time_point m_stop;
+};
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        std::cout << "Usage: smoothing <path_to_obj_file>";
+        return 0;            
+    }
+
     glow::glfw::GlfwContext ctx;
 
     auto g = gv::grid();
 
     OpenMesh::TriMesh mesh;
 
-    OpenMesh::IO::read_mesh(mesh, DATA_PATH.string() + "/" + "bunnyhead.obj");
+    //OpenMesh::IO::read_mesh(mesh, DATA_PATH.string() + "/" + "bunnyhead.obj");
+    OpenMesh::IO::read_mesh(mesh, std::string(argv[1]));
     glow_view_mesh(mesh, true, "Input Mesh");
 
 
@@ -60,18 +95,23 @@ int main()
         return mesh.property(v_pos, v);
         });
 
-    int max_iter = 100;
+    CPUTimer timer;
+
+    int num_iterations = 100;
     double learning_rate = 0.005;
-    for (int i = 0; i < max_iter; ++i) {
+
+    timer.start();
+    for (int i = 0; i < num_iterations; ++i) {
 
         auto [f, g] = func.eval_with_gradient(x);
 
-        std::cout << "f = " << f << "\n";
-
+        //std::cout << "f = " << f << "\n";
         //take a step         
         x = x - learning_rate * g;
-
     }
+    timer.stop();
+
+    std::cout << "\nSmoothing TinyAD: " << timer.elapsed_millis() << " (ms)," << timer.elapsed_millis() / float(num_iterations) << " ms per iteration\n";
 
     func.x_to_data(x, [&](OpenMesh::SmartVertexHandle v, const Eigen::Vector3d& _p) {
         mesh.point(v) = _p;
